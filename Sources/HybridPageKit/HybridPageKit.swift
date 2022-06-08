816581e57f15df422b6c86381d0a86f6b29c6789
tramp <wanggang3@staff.sina.com.cn>
2022-06-08 09:15:31 +0800

    update => add swift package manager

---
+++ Sources/HybridPageKit/HybridPageKit.swift
@@ -0,0 +1,6 @@
+public struct HybridPageKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/HybridPageKitTests/HybridPageKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import HybridPageKit
+
+final class HybridPageKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(HybridPageKit().text, "Hello, World!")
+    }
+}
