b61f61ff7fc8492ae42fe842c2c65cb519c87da3
okferret <wanggang3@staff.sina.com.cn>
2024-12-13 12:48:09 +0800

    update => no message

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "HybridPageKit",
-                path: "HybridPageKit/HybridPageKit/HybridPageKit",
+                path: "Sources/HybridPageKit",
                 exclude: ["Info.plist"],
                 publicHeadersPath: ".",
                 cSettings: [.headerSearchPath(".")]
